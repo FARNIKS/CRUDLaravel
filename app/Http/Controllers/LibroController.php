7bd6cc625d947cc60782b87930c15757edfa25e8
Miguel Angel Jimenez Fajardo <128429185+FARNIKS@users.noreply.github.com>
2026-02-26 16:49:13 -0500

    Se agrega toda la logica de laravel junto con bosstrap, para el crud este fue muy automatizado toca ver uno mas paso a paso y mas complejo

---
+++ app/Http/Controllers/LibroController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Libro;
+use App\Models\Categoria;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Http\Requests\LibroRequest;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class LibroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): View
+    {
+        $libros = Libro::paginate();
+
+        return view('libro.index', compact('libros'))
+            ->with('i', ($request->input('page', 1) - 1) * $libros->perPage());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        $libro = new Libro();
+        $categorias = Categoria::pluck('nombre', 'id');
+
+        return view('libro.create', compact('libro', 'categorias'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(LibroRequest $request): RedirectResponse
+    {
+        Libro::create($request->validated());
+
+        return Redirect::route('libros.index')
+            ->with('success', 'Libro created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id): View
+    {
+        $libro = Libro::find($id);
+
+        return view('libro.show', compact('libro'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id): View
+    {
+        $libro = Libro::find($id);
+        // fetch categories for the dropdown just like in create()
+        $categorias = Categoria::pluck('nombre', 'id');
+
+        return view('libro.edit', compact('libro', 'categorias'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(LibroRequest $request, Libro $libro): RedirectResponse
+    {
+        $libro->update($request->validated());
+
+        return Redirect::route('libros.index')
+            ->with('success', 'Libro updated successfully');
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        Libro::find($id)->delete();
+
+        return Redirect::route('libros.index')
+            ->with('success', 'Libro deleted successfully');
+    }
+}
